353bce48d2c86428a55ca50e706c774889dac15d
David Kim <davidhk21@gmail.com>
2021-02-02 02:56:44 -0700

    added contact section background photo

--- components/home_page_components/contact.js
+++ components/home_page_components/contact.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import styles from '../../styles/Home.module.css';
 
 export default function Contact() {
   return (
-    <div>
+    <div className={styles.contactContainer}>
+      <Image
+        src="/computer_background.JPG"
+        alt="Picture of someone working on a computer"
+        className={styles.contactBackgroundImage}
+      />
       Hello Contact!
     </div>
   );
