6419ba95be1493d08e6a60f9ba7d4d5f7698acb9
David Kim <davidhk21@gmail.com>
2021-02-01 21:39:31 -0700

    dynamically styled projects section

--- components/home_page_components/projects.js
+++ components/home_page_components/projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from '../../styles/Home.module.css';
 import { Card, Button } from 'react-bootstrap';
+import styles from '../../styles/Home.module.css';
 
 export default function Projects() {
   return (
