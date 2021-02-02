da351b4b8f75a623b42c24eeb2f8ad48ecdbd975
David Kim <davidhk21@gmail.com>
2021-02-01 19:51:00 -0700

    fixed passion section to be window responsive

--- components/home_page_components/about_me.js
+++ components/home_page_components/about_me.js
@@ -5,26 +5,19 @@ export default function AboutMe() {
   return (
     <div className={styles.aboutMeContainer}>
       <h1>A little bit about myself...</h1>
-      <div>
-        <h3 className={styles.aboutMeCategories}>Activities</h3>
-        <ul>
-          <li>Volleyball</li>
-          <li>Basketball</li>
-        </ul>
-      </div>
-      <div>
-        <h3 className={styles.aboutMeCategories}>Favorite Foods and Drinks</h3>
-        <ul>
-          <li>KBBQ</li>
-          <li>Classic Milk Tea</li>
-        </ul>
-      </div>
-      <div>
-        <h3 className={styles.aboutMeCategories}>Guilty Pleasures</h3>
-        <ul>
-          <li>&quot;The Office&quot;</li>
-          <li>Sweet Potato Cake</li>
-        </ul>
+      <div className={styles.aboutMeInfoContainer}>
+        <div className={styles.aboutMeDescContainer}>
+          <h3 className={styles.aboutMeCategories}>Hands</h3>
+          <p>&quot;As the body moves, the mind grooves&quot;. I love being active and you can most likely catch me at a volleyball open gym.</p>
+        </div>
+        <div className={styles.aboutMeDescContainer}>
+          <h3 className={styles.aboutMeCategories}>Heart</h3>
+          <p>I believe that a person&apos;s opinion of the world is also a confession of character.</p>
+        </div>
+        <div className={styles.aboutMeDescContainer}>
+          <h3 className={styles.aboutMeCategories}>Head</h3>
+          <p>Though I&apos;m confident in my knowledge, skills, and abilities, I recognize that I will always be a work in progress.</p>
+        </div>
       </div>
     </div>
   );
