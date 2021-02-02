05059efc710408d57edd35f5aeb3d754cad648ba
David Kim <davidhk21@gmail.com>
2021-02-01 21:29:16 -0700

    added project cards with title and descriptions

---
+++ components/home_page_components/projects.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from '../../styles/Home.module.css';
+import { Card, Button } from 'react-bootstrap';
+
+export default function Projects() {
+  return (
+    <div className={styles.projectsContainer}>
+      <h1 className={styles.projectsTitle}>Projects</h1>
+      <div className={styles.cardContainer}>
+        <Card className={styles.card}>
+          <Card.Img variant="top" src="prof_pic.jpg" />
+          <Card.Body>
+            <Card.Title>Homents</Card.Title>
+            <Card.Text>
+              A dynamic listing page of a real estate e-platform, built with the MERN stack.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className={styles.card}>
+          <Card.Img variant="top" src="prof_pic.jpg" />
+          <Card.Body>
+            <Card.Title>NearMiss</Card.Title>
+            <Card.Text>
+              Scaled and redesigned the backend of a digital retail marketplace for users to search, purchase, and review products
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className={styles.card}>
+          <Card.Img variant="top" src="prof_pic.jpg" />
+          <Card.Body>
+            <Card.Title>Pluck</Card.Title>
+            <Card.Text>
+              A cross-platform mobile application built with React-Native to help users increase productivity while helping them reach personal financial goals.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 import Introduction from '../components/home_page_components/introduction';
 import Passion from '../components/home_page_components/passion';
 import AboutMe from '../components/home_page_components/about_me';
+import Projects from '../components/home_page_components/projects';
 
 export default function Home() {
   return (
@@ -18,7 +18,7 @@ export default function Home() {
         <Introduction />
         <Passion />
         <AboutMe />
-
+        <Projects />
       </main>
 
     </div>
