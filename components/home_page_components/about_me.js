b88a966282619135be40a9f7d6c1dec6a25f5acf
David Kim <davidhk21@gmail.com>
2021-02-01 20:30:39 -0700

    used max function instead of media query for font size dynamic rendering

--- components/home_page_components/about_me.js
+++ components/home_page_components/about_me.js
@@ -8,15 +8,15 @@ export default function AboutMe() {
       <div className={styles.aboutMeInfoContainer}>
         <div className={styles.aboutMeDescContainer}>
           <h3 className={styles.aboutMeCategories}>Hands</h3>
-          <p>&quot;As the body moves, the mind grooves&quot;. I love being active and you can most likely catch me at a volleyball open gym.</p>
+          <p className={styles.aboutMeDescText}>&quot;As the body moves, the mind grooves&quot;. I love being active and you can most likely catch me at a volleyball open gym.</p>
         </div>
         <div className={styles.aboutMeDescContainer}>
           <h3 className={styles.aboutMeCategories}>Heart</h3>
-          <p>I believe that a person&apos;s opinion of the world is also a confession of character.</p>
+          <p className={styles.aboutMeDescText}>I believe that a person&apos;s opinion of the world is also a confession of character.</p>
         </div>
         <div className={styles.aboutMeDescContainer}>
           <h3 className={styles.aboutMeCategories}>Head</h3>
-          <p>Though I&apos;m confident in my knowledge, skills, and abilities, I recognize that I will always be a work in progress.</p>
+          <p className={styles.aboutMeDescText}>Though I&apos;m confident in my knowledge, skills, and abilities, I recognize that I will always be a work in progress.</p>
         </div>
       </div>
     </div>
