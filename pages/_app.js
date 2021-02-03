8fcad9c7973d0e8cc6f34e8d81eb8cb8dbec129b
David Kim <davidhk21@gmail.com>
2021-02-03 01:01:20 -0700

    CSS reset for different browsers

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 import NavBar from '../components/navbar';
 import Footer from '../components/footer';
