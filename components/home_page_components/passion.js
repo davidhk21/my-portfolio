8dcf9f41d8c128935c1de77c7bf8a3b9f7dfdcf0
David Kim <davidhk21@gmail.com>
2021-02-04 17:44:12 -0700

    added fade effect for passion and project sections of homepage

--- components/home_page_components/passion.js
+++ components/home_page_components/passion.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
 import styles from '../../styles/Home.module.css';
 
 export default function Passion() {
   return (
     <div className={styles.passionContainer1}>
-      <div className={styles.selfPortraitContainer}>
-        <Image
-          src="/Me.png"
-          alt="Picture of the author"
-          className={styles.selfPortrait}
-        />
-      </div>
-      <div className={styles.passionInfoContainer}>
-        <h1 className={styles.passionTitle}>My passion and focus is people and self-development</h1>
-        <p className={styles.passionDesc}>By pushing myself to have experiences such as studying abroad for a full year in Shanghai, China and organizing my University&apos;s very first TEDx event with the theme &quot;Take The Leap&quot;, I was able to develop a growth mindset that ultimately led me to fall in love with Software Engineering.</p>
-      </div>
+      <Fade distance="100px" delay={500} left>
+        <div className={styles.selfPortraitContainer}>
+          <Image
+            src="/Me.png"
+            alt="Picture of the author"
+            className={styles.selfPortrait}
+          />
+        </div>
+      </Fade>
+      <Fade distance="100px" delay={500} right>
+        <div className={styles.passionInfoContainer}>
+          <h1 className={styles.passionTitle}>My passion and focus is people and self-development</h1>
+          <p className={styles.passionDesc}>By pushing myself to have experiences such as studying abroad for a full year in Shanghai, China and organizing my University&apos;s very first TEDx event with the theme &quot;Take The Leap&quot;, I was able to develop a growth mindset that ultimately led me to fall in love with Software Engineering.</p>
+        </div>
+      </Fade>
     </div>
   );
 }
--- components/home_page_components/projects.js
+++ components/home_page_components/projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
 import Link from 'next/link';
 import styles from '../../styles/Home.module.css';
 
@@ -7,35 +8,37 @@ export default function Projects() {
   return (
     <div className={styles.projectsContainer}>
       <h1 className={styles.projectsTitle}>Projects</h1>
-      <div className={styles.cardContainer}>
-        <Card className={styles.card}>
-          <Card.Img variant="top" src="PhotoGallery.png" className={styles.cardImage} />
-          <Card.Body>
-            <Card.Title>Homents</Card.Title>
-            <Card.Text>
-              A dynamic listing page of a real estate e-platform, built with the MERN stack.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className={styles.card}>
-          <Card.Img variant="top" src="system-design.jpg" className={styles.cardImage} />
-          <Card.Body>
-            <Card.Title>NearMiss</Card.Title>
-            <Card.Text>
-              Scaled and redesigned the backend of a digital retail marketplace for users to search, purchase, and review products.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card className={styles.card}>
-          <Card.Img variant="top" src="pluck-logo.png" className={`${styles.cardImage} ${styles.pluckImage}`} />
-          <Card.Body>
-            <Card.Title>Pluck</Card.Title>
-            <Card.Text>
-              A cross-platform mobile application built with React-Native to help users increase productivity while helping them reach personal financial goals.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
+      <Fade distance="100px" delay={500} bottom cascade>
+        <div className={styles.cardContainer}>
+          <Card className={styles.card}>
+            <Card.Img variant="top" src="PhotoGallery.png" className={styles.cardImage} />
+            <Card.Body>
+              <Card.Title>Homents</Card.Title>
+              <Card.Text>
+                A dynamic listing page of a real estate e-platform, built with the MERN stack.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className={styles.card}>
+            <Card.Img variant="top" src="system-design.jpg" className={styles.cardImage} />
+            <Card.Body>
+              <Card.Title>NearMiss</Card.Title>
+              <Card.Text>
+                Scaled and redesigned the backend of a digital retail marketplace for users to search, purchase, and review products.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card className={styles.card}>
+            <Card.Img variant="top" src="pluck-logo.png" className={`${styles.cardImage} ${styles.pluckImage}`} />
+            <Card.Body>
+              <Card.Title>Pluck</Card.Title>
+              <Card.Text>
+                A cross-platform mobile application built with React-Native to help users increase productivity while helping them reach personal financial goals.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </Fade>
       <Link href="/portfolio">
         <a className={styles.buttonSeeProjects}>SEE PROJECTS</a>
       </Link>
