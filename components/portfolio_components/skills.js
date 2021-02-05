4ac8b0bfce5335f34e48b1883e99c085261bbb2d
David Kim <davidhk21@gmail.com>
2021-02-04 20:15:05 -0700

    skills layout set

---
+++ components/portfolio_components/skills.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import styles from '../../styles/Portfolio.module.css';
+
+export default function Skills() {
+  return (
+    <div className={styles.skillsContainer}>
+      <div className={styles.skillsListContainer}>
+        <div>
+          <p>TECHNOLOGIES</p>
+        </div>
+        <div>
+          <p>FRONT-END</p>
+          <ul>
+            <li>JavaScript (ES5, ES6+)</li>
+            <li>React.js</li>
+            <li>React-Native</li>
+            <li>Babel</li>
+            <li>Webpack</li>
+            <li>jQuery</li>
+            <li>CSS3</li>
+            <li>HTML5</li>
+            <li>Victory.js</li>
+          </ul>
+        </div>
+        <div>
+          <p>BACK-END</p>
+          <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>MongoDB</li>
+            <li>MySQL</li>
+            <li>PostgreSQL</li>
+            <li>Cassandra</li>
+            <li>Mongoose</li>
+            <li>Sequelize</li>
+            <li>NGINX</li>
+          </ul>
+        </div>
+        <div>
+          <p>DEV TOOLS & TECHNIQUES</p>
+          <ul>
+            <li>AWS EC2/S3</li>
+            <li>Jest</li>
+            <li>Chai</li>
+            <li>Mocha</li>
+            <li>Git</li>
+            <li>SOA</li>
+            <li>Agile Workflow</li>
+            <li>Remote Pair Programming</li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  );
+}
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import PortfolioIntro from '../components/portfolio_components/portfolio_intro';
+import Skills from '../components/portfolio_components/skills';
 
 export default function Portfolio() {
   return (
     <div>
       <PortfolioIntro />
+      <Skills />
     </div>
   );
 }
