791a68786c67e7f495769f119211dbbe81857675
David Kim <davidhk21@gmail.com>
2021-02-05 22:56:37 -0700

    refactored to use one class for picture containers

--- components/about_components/pictures.js
+++ components/about_components/pictures.js
@@ -5,42 +5,42 @@ import styles from '../../styles/About.module.css';
 export default function Pictures() {
   return (
     <div className={styles.picturesContainer}>
-      <div className={styles.picture1Container}>
+      <div className={`${styles.picture1Container} ${styles.pictureContainer}`}>
         <Image
           src="/1.jpg"
           alt="Picture of the author"
           className={styles.picture}
         />
       </div>
-      <div className={styles.picture2Container}>
+      <div className={`${styles.picture2Container} ${styles.pictureContainer}`}>
         <Image
           src="/2.jpg"
           alt="Picture of the author"
           className={styles.picture}
         />
       </div>
-      <div className={styles.picture3Container}>
+      <div className={`${styles.picture3Container} ${styles.pictureContainer}`}>
         <Image
           src="/3.jpg"
           alt="Picture of the author"
           className={styles.picture}
         />
       </div>
-      <div className={styles.picture4Container}>
+      <div className={`${styles.picture4Container} ${styles.pictureContainer}`}>
         <Image
           src="/4.jpg"
           alt="Picture of the author"
           className={styles.picture}
         />
       </div>
-      <div className={styles.picture5Container}>
+      <div className={`${styles.picture5Container} ${styles.pictureContainer}`}>
         <Image
           src="/5.jpg"
           alt="Picture of the author"
           className={styles.picture}
         />
       </div>
-      <div className={styles.picture6Container}>
+      <div className={`${styles.picture6Container} ${styles.pictureContainer}`}>
         <Image
           src="/6.jpg"
           alt="Picture of the author"
