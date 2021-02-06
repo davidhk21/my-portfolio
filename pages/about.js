20ed5f30a2e18590b225244ce8d6ca5aa5bbdb0c
David Kim <davidhk21@gmail.com>
2021-02-05 20:40:51 -0700

    created fun facts component

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import AboutIntro from '../components/about_components/about_intro';
+import FunFacts from '../components/about_components/fun_facts';
 
 export default function About() {
   return (
     <div>
       <AboutIntro />
+      <FunFacts />
     </div>
   );
 }
