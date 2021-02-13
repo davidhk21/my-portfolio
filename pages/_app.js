e2be20fe3038ee92d0a0e7639904e92fb0631de0
David Kim <davidhk21@gmail.com>
2021-02-12 23:28:42 -0700

    added mobile meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -15,6 +16,9 @@ library.add(fab, far, fas);
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <meta name="viewport" content ="width=device-width,initial-scale=1,user-scalable=yes" />
+      </Head>
       <NavBar />
       <Component {...pageProps} />
       <Footer />
