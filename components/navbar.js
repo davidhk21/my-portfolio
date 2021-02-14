87a583de6943c430177b92337fd761c52ceac2df
David Kim <davidhk21@gmail.com>
2021-02-13 22:09:49 -0700

    navbar cleanup

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
 import { Navbar, Nav } from 'react-bootstrap';
 import styles from '../styles/App.module.css';
 
@@ -16,30 +15,6 @@ export default function NavBar() {
   });
 
   return (
-    // <div className={styles.navBarContainer}>
-    //   <Navbar.Brand href="/" className={styles.brand}>
-    //     David Kim
-    //   </Navbar.Brand>
-    //   <Navbar variant="dark">
-    //     <Nav className={styles.navLinks}>
-    //       <Link href="/" passHref>
-    //         <a className={styles.navLink}>HOME</a>
-    //       </Link>
-    //       <Link href="/portfolio" passHref>
-    //         <a className={styles.navLink}>PORTFOLIO</a>
-    //       </Link>
-    //       <Link href="/about" passHref>
-    //         <a className={styles.navLink}>ABOUT</a>
-    //       </Link>
-    //     </Nav>
-    //   </Navbar>
-    //   <div className={styles.contactContainer}>
-    //     <Link href="/contact_me" passHref>
-    //       <a className={styles.navLink}>CONTACT</a>
-    //     </Link>
-    //   </div>
-    // </div>
-
     <div className={styles.navBarContainer}>
       <Navbar expand="md" variant={width > 767 ? 'dark' : 'light'} bg={width > 767 ? '' : 'light'}>
         <Navbar.Brand href="/" className={styles.brand}>
