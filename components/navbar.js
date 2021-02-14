dae470290ced3ab72ed04c1d7aa32a14fe4c701c
David Kim <davidhk21@gmail.com>
2021-02-13 21:59:41 -0700

    finished navbar

--- components/navbar.js
+++ components/navbar.js
@@ -41,25 +41,25 @@ export default function NavBar() {
     // </div>
 
     <div className={styles.navBarContainer}>
-      <Navbar expand="sm" variant={width > 575 ? 'dark' : 'light'} bg={width > 575 ? '' : 'light'}>
+      <Navbar expand="md" variant={width > 767 ? 'dark' : 'light'} bg={width > 767 ? '' : 'light'}>
         <Navbar.Brand href="/" className={styles.brand}>
           David Kim
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className={styles.navbar}>
           <Nav className="ml-auto">
-            <Link href="/" passHref>
-              <a className={styles.navLink}>HOME</a>
-            </Link>
-            <Link href="/portfolio" passHref>
-              <a className={styles.navLink}>PORTFOLIO</a>
-            </Link>
-            <Link href="/about" passHref>
-              <a className={styles.navLink}>ABOUT</a>
-            </Link>
-            <Link href="/contact_me" passHref>
-              <a className={styles.navLink}>CONTACT</a>
-            </Link>
+            <Nav.Link href="/">
+              <p className={styles.navLink}>HOME</p>
+            </Nav.Link>
+            <Nav.Link href="/portfolio">
+              <p className={styles.navLink}>PORTFOLIO</p>
+            </Nav.Link>
+            <Nav.Link href="/about">
+              <p className={styles.navLink}>ABOUT</p>
+            </Nav.Link>
+            <Nav.Link href="/contact_me">
+              <p className={styles.navLink}>CONTACT</p>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <meta name="viewport" content ="width=device-width,initial-scale=1,user-scalable=yes" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=yes" />
       </Head>
       <NavBar />
       <Component {...pageProps} />
