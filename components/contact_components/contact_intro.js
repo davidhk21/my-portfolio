a5b1ef0effb7dc82881a1075ff61b27009623b79
David Kim <davidhk21@gmail.com>
2021-02-20 22:34:48 -0700

    added cdn images for contact page

--- components/contact_components/contact_intro.js
+++ components/contact_components/contact_intro.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
 import styles from '../../styles/Contact.module.css';
+import { imageUrl } from '../../pages/_app';
 
 export default function ContactIntro() {
   return (
     <div className={styles.imageContainer}>
       <Image
-        src="/contact-background.jpg"
+        src={`${imageUrl}/contact-background.jpg`}
         alt="Picture of the author"
         className={styles.introImage}
         fluid
